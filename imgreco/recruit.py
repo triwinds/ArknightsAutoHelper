1d06c7bc36a6974c36ebd2c18c74c3a2eb9b16c4
triwinds <triwinds@users.noreply.github.com>
2022-02-11 11:40:30 +0800

    add preprocess in recruit

--- imgreco/recruit.py
+++ imgreco/recruit.py
@@ -1,3 +1,4 @@
+import cv2
 import cv2 as cv
 import numpy as np
 from PIL import Image
@@ -7,6 +8,7 @@ from . import imgops
 from . import resources
 from . import util
 from . import ocr
+from imgreco.common import convert_to_cv, convert_to_pil
 
 LOGFILE = 'recruit.html'
 logger = get_logger(__name__)
@@ -26,6 +28,22 @@ def get_recruit_tags(screen):
     return list(res.keys())
 
 
+def crop_image_only_outside(gray_img, raw_img, threshold=128, padding=3):
+    mask = gray_img > threshold
+    m, n = gray_img.shape
+    mask0, mask1 = mask.any(0), mask.any(1)
+    col_start, col_end = mask0.argmax(), n - mask0[::-1].argmax()
+    row_start, row_end = mask1.argmax(), m - mask1[::-1].argmax()
+    return raw_img[max(0, row_start - padding):min(m, row_end + padding),
+           max(0, col_start - padding):min(n, col_end + padding)]
+
+
+def preprocess(pil_img):
+    cv_img = convert_to_cv(pil_img, cv2.COLOR_BGR2GRAY)
+    cv_img = crop_image_only_outside(cv_img, cv_img)
+    return Image.fromarray(cv_img)
+
+
 def get_recruit_tags_with_rect(screen):
     import textdistance
     vw, vh = util.get_vwvh(screen)
@@ -41,8 +59,9 @@ def get_recruit_tags_with_rect(screen):
     recognize = lambda img: eng.recognize(imgops.invert_color(img), int(vh * 20), hints=[ocr.OcrHint.SINGLE_LINE], char_whitelist=known_tagchars).text.replace(' ', '')
     cookedtags = {}
     for tag_rect in tag_rects:
-        img = screen.crop(tag_rect).convert('L')
+        img = screen.crop(tag_rect)
         logger.logimage(img)
+        img = preprocess(img)
         tag = recognize(img)
         logger.logtext(tag)
         if not tag:
