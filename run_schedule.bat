5f44158e423688bf579539c575b06c9a0e77d7a6
triwinds <triwinds@users.noreply.github.com>
2023-01-10 21:35:25 +0800

    check permissions before run schedule

--- run_schedule.bat
+++ run_schedule.bat
@@ -1,4 +1,31 @@
 @echo off
+REM --add the following to the top of your bat file--
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    @REM pushd "%CD%"
+    @REM CD /D "%~dp0"
+:--------------------------------------
+
 chcp>nul 2>nul 65001
 
 rem 切换至 ArknightsAutoHelper 所在位置
